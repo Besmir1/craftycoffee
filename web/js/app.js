f63585b6a38707622ac65297dc6322e483a2b0f0
besmirgashi <bg38721@ubt-uni.net>
2023-06-09 09:21:54 +0200

    GET, POST, PUT & DELETE using AJAX in JavaScript

--- web/js/app.js
+++ web/js/app.js
@@ -1,3 +1,19 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+// document.getElementById('load-button').addEventListener('click', function() {
+//     var form = document.getElementById('my-form');
+//     var formData = new FormData(form);
+
+//     var xhr = new XMLHttpRequest();
+//     xhr.onreadystatechange = function() {
+//       if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+//         // Update content on successful response
+//         document.getElementById('result').innerHTML = xhr.responseText;
+//         console.log(xhr.responseText )
+//       }
+//     };
+//     xhr.open('POST', 'templates/index', true);
+//     xhr.send(formData);
+// });
